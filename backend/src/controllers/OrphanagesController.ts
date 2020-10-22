3407240f25d5542f0168ea237eda61bfd10db677
stilviopedro <stilviopedro15@gmail.com>
2020-10-22 18:42:39 +0100

    Guardando o nome das images na base de dados

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
-import Orphanage from '../models/Orphanages';
+import Orphanage from '../models/Orphanage';
 
 export default{
     async index(request: Request, response: Response){
@@ -33,6 +33,11 @@ export default{
         } = request.body;
     
         const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
     
         const orphanage = orphanagesRepository.create({
             name,
@@ -42,6 +47,7 @@ export default{
             instructions,
             opening_hours,
             open_on_weekends,
+            images
         })
         
         await orphanagesRepository.save(orphanage);
