691d5e1513d563126cfa7c58915bee9457875eb7
stilviopedro <stilviopedro15@gmail.com>
2020-10-22 18:16:30 +0100

    upload de imagens finalizado

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,40 +1,14 @@
-import express, { request, response } from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanages';
-
+import express from 'express';
 import './database/connection';
-import Orphanages from './models/Orphanages';
 
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
 
-app.post('/orphanages', async (request, response) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    } = request.body;
+app.use(routes);
 
-    const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    })
-    
-    await orphanagesRepository.save(orphanage);
-
-    return response.status(201).json(orphanage);
-});
 
 app.listen(3333);
