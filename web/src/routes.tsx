e2061481c5642b7853e3e29f28941f60255e50cd
stilviopedro <stilviopedro15@gmail.com>
2020-10-24 00:35:58 +0100

    Terminei a pagina web

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
-import OrphanageMap from './pages/OrphanageMap'
+import OrphanageMap from './pages/OrphanageMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes() {
     return (
@@ -10,6 +12,9 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={OrphanageMap} />
+
+                <Route path="/orphanages/create" exact component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} />
             </Switch>
         </BrowserRouter>
     );
